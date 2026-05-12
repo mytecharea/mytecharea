1b5ac62d6b36d8d474a8e240cfd3eac1c0c62549
niraj <kumarniraj543@gmail.com>
2026-05-12 06:29:14 +0530

    feat: add footer component with template, styles, and tests

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar.component';
+import { Footer } from "./pages/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
