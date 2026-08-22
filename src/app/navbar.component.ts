19325145876647f41b24f8974bd92d6e9df492a1
niraj <kumarniraj543@gmail.com>
2026-08-22 17:49:01 +0530

    feat: add "Manage Task" link to the navbar for task dashboard navigation

--- src/app/navbar.component.ts
+++ src/app/navbar.component.ts
@@ -54,6 +54,13 @@ import { RouterLink } from '@angular/router';
           </svg>
           Contact
         </a>
+
+         <a href="/task-dashboard/" class="nav-item">
+          <svg class="nav-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+            <path d="M3 8l7.89 4.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"/>
+          </svg>
+          Manage Task
+        </a>
       </nav>
     </header>
   `,
